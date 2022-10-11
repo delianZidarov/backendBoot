97f1b16c25db66746df88d14856fa2db46d25e1a
Delian Zidarov <d.zidarov@gmail.com>
2022-10-11 20:17:24 +0900

    passport local

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const session = require("express-session");
 const FileStore = require("session-file-store")(session);
+const passport = require("passport");
+
+const authenticate = require("./authenticate");
 
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
@@ -52,19 +55,16 @@ app.use(
   })
 );
 
+app.use(passport.initialize());
+app.use(passport.session());
+
 function auth(req, res, next) {
-  if (!req.session.user) {
+  if (!req.user) {
     const err = new Error("You are not authenticated!");
     err.status = 401;
     return next(err);
   } else {
-    if (req.session.user === "authenticated") {
-      return next();
-    } else {
-      const err = new Error("You are not authenticated!");
-      err.status = 401;
-      return next(err);
-    }
+    return next();
   }
 }
 
